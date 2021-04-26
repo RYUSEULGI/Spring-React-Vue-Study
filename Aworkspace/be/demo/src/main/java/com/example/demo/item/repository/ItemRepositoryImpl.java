7f25bee7af6230ca2729a34568897ac92752c195
RYUSEULGI <71699667+RYUSEULGI@users.noreply.github.com>
2021-04-26 15:32:40 +0900

    Delete Aworkspace directory

--- Aworkspace/be/demo/src/main/java/com/example/demo/item/repository/ItemRepositoryImpl.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.item.repository;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class ItemRepositoryImpl implements ItemCustomRepository {
-
-
-}
--- Aworkspace/be/demo/src/main/java/com/example/demo/item/service/ItemService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.item.service;
-
-import com.example.demo.item.domain.Item;
-
-public interface ItemService{
-	public void create(Item item);
-	public Item detail(long itemNo);
-	public long update(long id, Item item);
-}
